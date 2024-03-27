687dc52d0e67af6950039a155cdc59458f715e10
pisesanket <85410203+pisesanket@users.noreply.github.com>
2024-03-27 10:13:45 +0530

    Update mapLocation.jsx

--- src/mapLocation.jsx
+++ src/mapLocation.jsx
@@ -291,7 +291,7 @@ const MapContainer = () => {
         
       )}
       <div>
-        {coordinates&&<MapWithPolyline coordinates={coordinates} />}
+        {coordinates&&<MapWithPolyline coordinates={cords} />}
         
       </div>
     </div>
