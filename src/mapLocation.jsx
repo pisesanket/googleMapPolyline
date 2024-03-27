4a602175a2f93195381192d1f803c12da56b0c2f
pisesanket <85410203+pisesanket@users.noreply.github.com>
2024-03-27 14:56:24 +0530

    Update mapLocation.jsx

--- src/mapLocation.jsx
+++ src/mapLocation.jsx
@@ -83,7 +83,7 @@ const MapContainer = () => {
               // fetchPlaceData(position.coords.latitude, position.coords.longitude);
               console.log(distance);
               if (distance >= MOVEMENT_THRESHOLD) {
-                pushUserLocation("30", newPosition);
+                pushUserLocation("40", newPosition);
                 setSpeed(position.coords.speed);
                 setAccuracy(position.coords.accuracy);
                 setHeading(position.coords.heading);
@@ -306,7 +306,7 @@ const fetchUserLocationsContinuous = (userId) => {
           <p>Speed: {speed}</p>
           <p>Accuracy: {accuracy}</p>
           <p>Heading: {heading}</p>
-          <p onClick={()=>{fetchUserLocationsContinuous('30');}}>Request Count: {requestCount}</p>
+          <p onClick={()=>{fetchUserLocationsContinuous('40');}}>Request Count: {requestCount}</p>
         </div>
         
       )}
